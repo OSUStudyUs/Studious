ea658d0d0a9f2bc4a96a4de1349ada779b613abb
Alex Tareshawty <atareshawty@gmail.com>
2016-11-18 16:09:01 -0500

    client: api util

---
+++ client/src/utils/api.js
@@ -0,0 +1,30 @@
+import 'whatwg-fetch';
+
+// TODO: actually check environment variable
+const baseUrl = 'http://localhost:8080';
+
+const request = (url, { method, body }) =>
+  fetch(`${baseUrl}/api${url}`, {
+    headers: { // eslint-disable-line quote-props
+      Accept: 'application/json, text/html',
+      'Access-Control-Allow-Origin': '*',
+      Authorization: localStorage.userToken && `Bearer ${localStorage.userToken}`,
+      'Content-Type': 'application/json'
+    },
+    method,
+    body: JSON.stringify(body)
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw response.status;
+      }
+
+      return response.json()
+    })
+
+const del = (url) => request(url, { method: 'delete' });
+const get = (url) => request(url, { method: 'get' });
+const post = (url, body) => request(url, { method: 'post', body });
+const put = (url, body) => request(url, { method: 'put', body });
+
+export default { del, get, post, put };
