1abb4290be24cfb46c784b5859d59d75acf71f4c
Alex Tareshawty <atareshawty@gmail.com>
2016-11-18 16:10:20 -0500

    client: app.js -> app.jsx with routing

---
+++ client/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter, Match, Redirect } from 'react-router';
+
+import './App.css';
+import { Home, LandingPage, Navbar } from './shared_components';
+
+
+const mapStateToProps = ({ authentication }) => ({
+  isAuthenticated: authentication.isAuthenticated,
+  errorMessage: authentication.errorMessage
+});
+
+const MatchWhenAuthorized = ({ component: Component, isAuthenticated, pattern, ...rest }) => (
+  <Match {...rest} pattern={pattern} render={(props) => {
+    if (isAuthenticated) {
+      return <Component {...props} />;
+    } else {
+      return <Redirect to={{ pathname: '/', state: {from: props.location }}} />;
+    }
+  }}
+  />
+);
+
+const MatchWhenNotLoggedIn = ({ component: Component, pattern, ...rest }) => {
+  return (
+    <Match pattern={pattern} render={(props) => {
+        if (!rest.isAuthenticated) {
+          return <Component {...props} {...rest} />;
+        } else {
+          return <Redirect to={{ pathname: '/' }} />;
+        }
+    }}
+    />
+  )
+};
+
+const App = ({ dispatch, isAuthenticated, errorMessage, ...rest }) => {
+  return (
+  <BrowserRouter>
+    {
+      ({ router }) => (
+        <div className="App">
+          {typeof errorMessage !== 'undefined' && <p>{errorMessage}</p>}
+          <Navbar dispatch={dispatch} isAuthenticated={isAuthenticated} />
+          <MatchWhenNotLoggedIn component={LandingPage} dispatch={dispatch} isAuthenticated={isAuthenticated} pattern="/" />
+          <MatchWhenAuthorized component={Home} dispatch={dispatch} isAuthenticated={isAuthenticated} pattern="/" />
+        </div>
+      )
+    }
+  </BrowserRouter>
+)};
+
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string
+};
+
+export default connect(mapStateToProps)(App);
