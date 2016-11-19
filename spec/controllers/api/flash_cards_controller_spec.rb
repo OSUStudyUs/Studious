ed129e74f37a766a2da98d381836769922b76054
Kyle Thompson <kyle@kylekthompson.com>
2016-11-18 19:03:32 -0500

    remove bad tests

--- spec/controllers/api/flash_cards_controller_spec.rb
+++ spec/controllers/api/flash_cards_controller_spec.rb
@@ -2,25 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Api::FlashCardsController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
