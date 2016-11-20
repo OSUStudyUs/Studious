b241262a14106886700e5bf322d415dff9a485a2
Kyle Thompson <kyle@kylekthompson.com>
2016-11-20 15:01:51 -0500

    api: add helper method to StudyGroup for initial creation

--- app/models/study_group.rb
+++ app/models/study_group.rb
@@ -1,11 +1,37 @@
 class StudyGroup < ApplicationRecord
   # associations
-  belongs_to :course
+  belongs_to :course, optional: true
 
   has_many :flash_card_sets, dependent: :destroy
 
-  # validations
-  validates :course, presence: true
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
 
+  # validations
   validates :name, presence: true
+
+  # helper methods
+  def save_for(user)
+    successful = true
+
+    StudyGroup.transaction do
+      successful = self.save
+
+      if successful
+        membership = Membership.new(
+          user: user,
+          study_group: self,
+          role: :admin
+        )
+
+        unless membership.save
+          successful = false
+          raise ActiveRecord::Rollback, "Call tech support!"
+          self.errors[:membership] = membership.errors
+        end
+      end
+    end
+
+    return successful
+  end
 end
