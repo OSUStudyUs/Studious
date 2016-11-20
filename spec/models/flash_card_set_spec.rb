d0a506d0572c05b99479150b9a14322107e76c13
Kyle Thompson <kyle@kylekthompson.com>
2016-11-20 17:23:24 -0500

    api: add FlashCardSet spec

--- spec/models/flash_card_set_spec.rb
+++ spec/models/flash_card_set_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe FlashCardSet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid when created for a study group" do
+    expect(FactoryGirl.create(:flash_card_set)).to be_valid
+  end
+
+  it "is valid when created for a user" do
+    expect(FactoryGirl.create(:flash_card_set, :for_a_user)).to be_valid
+  end
+
+  it "is valid when it has flash_cards" do
+    expect(FactoryGirl.create(:flash_card_set, :with_flash_cards)).to be_valid
+  end
 end
