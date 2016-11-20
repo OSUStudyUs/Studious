887f8f5c1772724226f6f6a22e1b100d43cecf90
Kyle Thompson <kyle@kylekthompson.com>
2016-11-20 17:39:16 -0500

    api: add CourseUser spec

--- spec/models/course_user_spec.rb
+++ spec/models/course_user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CourseUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid when created with valid attributes" do
+    expect(FactoryGirl.create(:course_user)).to be_valid
+  end
+
+  it "is invalid when created without a user" do
+    membership = FactoryGirl.build(:course_user, :without_a_user)
+    membership.valid?
+    expect(membership.errors[:user]).to include("can't be blank")
+  end
+
+  it "is invalid when created without a course" do
+    membership = FactoryGirl.build(:course_user, :without_a_course)
+    membership.valid?
+    expect(membership.errors[:course]).to include("can't be blank")
+  end
 end
