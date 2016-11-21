6d3a1d4ff1b6128e659d4e4d2cb7fb5522db993a
Alex Tareshawty <atareshawty@gmail.com>
2016-11-21 02:03:12 -0500

    client: format rails in api.js to camelCase slightly format errors from api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,25 +1,42 @@
+import { camelCase } from 'change-case';
 import 'whatwg-fetch';
 
 const baseUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080';
 
 const request = (url, { method, body }) =>
-  fetch(`${baseUrl}/api${url}`, {
-    headers: { // eslint-disable-line quote-props
-      Accept: 'application/json, text/html',
-      Authorization: localStorage.userToken && `Bearer ${localStorage.userToken}`,
-      'Content-Type': 'application/json'
-    },
-    method,
-    mode: 'cors',
-    body: JSON.stringify(body)
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw response.status;
-      }
-
-      return response.json()
+  new Promise((resolve, reject) => {
+    fetch(`${baseUrl}/api${url}`, {
+      headers: { // eslint-disable-line quote-props
+        Accept: 'application/json, text/html',
+        Authorization: localStorage.userToken && `Bearer ${localStorage.userToken}`,
+        'Content-Type': 'application/json'
+      },
+      method,
+      mode: 'cors',
+      body: JSON.stringify(body)
     })
+      .then((response) => {
+        if (!response.ok) {
+          return Promise.reject(response.text());
+        }
+
+        return response.json();
+      })
+      .then((json) => resolve(json))
+      .catch((errors) => {
+        // errors from the rails api are coming to us in snakecase
+        // so we're converting to camelCase here before they get outside the actual fetching
+        errors.then((text) => {
+          const textJSON = JSON.parse(text).errors;
+          const newErrors = Object.keys(textJSON).reduce((prev, key) => {
+            prev[camelCase(key)] = textJSON[key];
+            return prev;
+          }, {});
+
+          reject(newErrors);
+        });
+      });
+  });
 
 const del = (url) => request(url, { method: 'delete' });
 const get = (url) => request(url, { method: 'get' });
