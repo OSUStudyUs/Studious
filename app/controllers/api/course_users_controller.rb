06252716c25472523d8fd2b0005aea0462d5ff24
Alex Tareshawty <atareshawty@gmail.com>
2016-11-22 10:37:03 -0500

    api: CourseUsersController#delete impl and tests

--- app/controllers/api/course_users_controller.rb
+++ app/controllers/api/course_users_controller.rb
@@ -1,13 +1,41 @@
 class Api::CourseUsersController < ApplicationController
-  # Author: TODO
+  before_action :authenticate_user
+  before_action :ensure_user_in_course!, except: [:create]
+
+  # Author: Alex Tareshawty
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
+  #   2: 11/22/16 - Alex Tareshawty - Initial Implementation
   def create
   end
 
-  # Author: TODO
+  # Author: Alex Tareshawty
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
+  #   2: 11/22/16 - Alex Tareshawty - Initial Implementation
   def destroy
+    course_user = CourseUser.find params[:id]
+
+    if course_user.destroy
+      head 204
+    else
+      render json: { errors: ["User could not be destroyed"] }, status: 500
+    end
+  end
+
+
+  private
+
+  # Private: enforces authorization such that the current_user belongs_to the course_user in question
+  #
+  # Author: Alex Tareshawty
+  # Revisions:
+  #   1: 11//22/16 - Alex Tareshawty - initial implementation
+  def ensure_user_in_course!
+    user_from_course_user = CourseUser.find params[:id]
+
+    unless current_user && user_from_course_user && current_user.id == user_from_course_user.id
+      head 401
+    end
   end
 end
--- spec/controllers/api/course_users_controller_spec.rb
+++ spec/controllers/api/course_users_controller_spec.rb
@@ -2,4 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Api::CourseUsersController, type: :controller do
 
+  describe "DELETE #destroy" do
+    let!(:course_user) { FactoryGirl.create(:course_user) }
+    let!(:other_course_user) { FactoryGirl.create(:course_user) }
+    let(:headers) { auth_header(course_user.user) }
+
+    context "when passed the correct user's token" do
+      it "has status 204" do
+        request.headers.merge! headers
+        delete :destroy, params: { id: course_user.id }, format: :json
+        expect(response).to have_http_status(204)
+      end
+
+      it "destroys the course_user" do
+        request.headers.merge! headers
+
+        expect {
+          delete :destroy, params: { id: course_user.id }, format: :json
+        }.to change(User, :count).by -1
+      end
+    end
+
+    context "when passed the wrong user's token" do
+      it "has status 401" do
+        request.headers.merge! headers
+        delete :destroy, params: { id: other_course_user }, format: :json
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context "when not passed a valid token" do
+      it "has status 401" do
+        delete :destroy, params: { id: course_user }, format: :json
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
 end
