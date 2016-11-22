3267a2dd35bc15c74e0f8132b26c08e301e88637
Joel Diener <jzdiener@gmail.com>
2016-11-21 22:04:54 -0500

    Removed flash card and flash card sets controller changes

--- app/controllers/api/flash_card_sets_controller.rb
+++ app/controllers/api/flash_card_sets_controller.rb
@@ -2,20 +2,13 @@ class Api::FlashCardSetsController < ApplicationController
   # Author: TODO
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
-  #   2: 11/21/16 - Joel Diener - initial implementation
   def index
-    @flash_card_set = FlashCardSet.all
-
-    render :index, status: 200
   end
 
   # Author: TODO
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
   def show
-    @user = FlashCardSet.find params[:id]
-
-    render :show, status: 200
   end
 
   # Author: TODO
--- app/controllers/api/flash_cards_controller.rb
+++ app/controllers/api/flash_cards_controller.rb
@@ -2,57 +2,18 @@ class Api::FlashCardsController < ApplicationController
   # Author: TODO
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
-  #   2: 11/21/16 - Joel Diener - initial implementation
   def create
-
   end
 
   # Author: TODO
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
-  #   2: 11/21/16 - Joel Diener - initial implementation
   def update
-    @flash_card = User.find params[:id]
-
-    if @flash_card.update_attributes update_params
-      render :show, status: 200
-    else
-      render json: { errors: @flash_card.errors }, status: 409
-    end
   end
 
   # Author: TODO
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
-  #   2: 11/21/16 - Joel Diener - initial implementation
   def destroy
-    flash_card = FlashCard.find params[:id]
-
-    if flash_card.destroy
-      head 204
-    else
-      render json: { errors: ["Flash card could not be destroyed"] }, status: 500
-    end
-  end
-
-  private
-  # Author: Joel Diener
-  # Revisions:
-  #   1: 11/21/16 - Joel Diener - initial implementation
-  def create_params
-    params.require(:flash_card).permit(
-      :email,
-      :first_name,
-      :last_name,
-      :password,
-      :password_confirmation
-    )
-  end
-
-  # Author: Joel Diener
-  # Revisions:
-  #   1: 11/21/16 - Joel Diener - initial implementation
-  def update_params
-    create_params
   end
 end
