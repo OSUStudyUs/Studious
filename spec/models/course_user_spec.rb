52d77593fa7928e679ffb28b1021d0cd5c830ca9
Alex Tareshawty <atareshawty@gmail.com>
2016-11-22 15:11:29 -0500

    api: add tests for CourseUser model validations

--- spec/models/course_user_spec.rb
+++ spec/models/course_user_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe CourseUser, type: :model do
     membership.valid?
     expect(membership.errors[:course]).to include("can't be blank")
   end
+
+  it "is invalid when a user tries to join a course again" do
+    membership_one = FactoryGirl.create(:course_user)
+    membership_two = FactoryGirl.build(:course_user, course_id: membership_one.course.id, user_id: membership_one.user.id)
+    expect(membership_two.valid?).to eq(false)
+    expect(membership_two.errors[:user]).to include("can only join course once")
+  end
 end
