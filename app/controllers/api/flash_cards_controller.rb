0bf9f41eeeb3d35c526c03e62c9e4e57a1fc8ec4
Joel Diener <jzdiener@gmail.com>
2016-11-25 20:55:59 -0500

    FlashCardsController: InitialImplementation

--- app/controllers/api/flash_cards_controller.rb
+++ app/controllers/api/flash_cards_controller.rb
@@ -1,19 +1,67 @@
 class Api::FlashCardsController < ApplicationController
-  # Author: TODO
+  before_action :authenticate_user, except: [:create]
+
+  # Author: Joel Diener
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
+  #   2: 11/21/16 - Joel Diener - initial implementation
   def create
+    @flash_card = FlashCard.new create_params
+
+    if params[:flash_card_set_id]
+      @flash_card.flash_card_set_id = params[:flash_card_set_id]
+    end
+
+    if @flash_card.save
+      render :show, status: 201
+    else
+      render json: { errors: @flash_card.errors }, status: 409
+    end
   end
 
-  # Author: TODO
+  # Author: Joel Diener
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
+  #   2: 11/21/16 - Joel Diener - initial implementation
   def update
+    @flash_card = FlashCard.find id: params[:id]
+
+    if @flash_card.update_attributes update_params
+      render :show, status: 200
+    else
+      render json: { errors: @flash_card.errors }, status: 409
+    end
   end
 
-  # Author: TODO
+  # Author: Joel Diener
   # Revisions:
   #   1: 11/17/16 - Kyle Thompson - skeleton
+  #   2: 11/21/16 - Joel Diener - initial implementation
   def destroy
+    flash_card = FlashCard.find params[:id]
+
+    if flash_card.destroy
+      head 204
+    else
+      render json: { errors: ["Flash card could not be destroyed"] }, status: 500
+    end
+  end
+
+  private
+  # Author: Joel Diener
+  # Revisions:
+  #   1: 11/21/16 - Joel Diener - initial implementation
+  def create_params
+    params.require(:flash_card).permit(
+      :question,
+      :answer
+    )
+  end
+
+  # Author: Joel Diener
+  # Revisions:
+  #   1: 11/21/16 - Joel Diener - initial implementation
+  def update_params
+    create_params
   end
 end
--- spec/controllers/api/flash_cards_controller_spec.rb
+++ spec/controllers/api/flash_cards_controller_spec.rb
@@ -1,5 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe Api::FlashCardsController, type: :controller do
+  describe "POST #create" do
+    let(:flash_card) { FactoryGirl.attributes_for(:flash_card) }
+    let(:incorrect_flash_card) { FactoryGirl.attributes_for(:flash_card, question: nil) }
+    let(:flash_card_set) { FactoryGirl.create(:flash_card_set) }
 
+    context "with correct attributes" do
+      it "has status 201" do
+        post :create, params: { flash_card_set_id: flash_card_set.id, flash_card: flash_card }, format: :json
+        expect(response).to have_http_status(201)
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it "saves the new flash card" do
+        expect {
+          post :create, params: { flash_card_set_id: flash_card_set.id, flash_card: flash_card }, format: :json
+        }.to change(FlashCard, :count).by 1
+      end
+
+      it "renders the show template" do
+        post :create, params: { flash_card_set_id: flash_card_set.id, flash_card: flash_card }, format: :json
+        expect(response).to render_template(:show)
+      end
+    end
+
+    context "with incorrect attributes" do
+      it "has status 409" do
+        post :create, params: { flash_card_set_id: flash_card_set.id, flash_card: incorrect_flash_card }, format: :json
+        expect(response).to have_http_status(409)
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it "doesn't save the new flash card" do
+        expect {
+          post :create, params: { flash_card_set_id: flash_card_set.id, flash_card: incorrect_flash_card }, format: :json
+        }.not_to change(FlashCard, :count)
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    let!(:user) { FactoryGirl.create(:user) }
+    let(:flash_card) { FactoryGirl.attributes_for(:flash_card) }
+    let!(:other_flash_card) { FactoryGirl.create(:flash_card) }
+    let(:different_flash_card) { FactoryGirl.attributes_for(:flash_card, question: "Test question") }
+    let(:invalid_flash_card) { FactoryGirl.attributes_for(:flash_card, question: nil) }
+    let(:headers) { auth_header(user) }
+
+    context "when passed the correct user's token" do
+      context "with valid attributes" do
+        it "has status 200" do
+          request.headers.merge! headers
+          patch :update, params: { id: flash_card, flash_card: different_flash_card }, format: :json
+          expect(response).to have_http_status(200)
+          expect(response.content_type).to eq("application/json")
+        end
+
+        it "assigns @flash_card" do
+          request.headers.merge! headers
+          patch :update, params: { id: flash_card, flash_card: different_flash_card }, format: :json
+          expect(assigns(:flash_card)).to eq flash_card
+        end
+
+        it "updates the flash card" do
+          request.headers.merge! headers
+          patch :update, params: { id: flash_card, flash_card: different_flash_card }, format: :json
+          user.reload
+          expect(user.question).to eq "Test question"
+        end
+
+        it "renders the show template" do
+          request.headers.merge! headers
+          patch :update, params: { id: flash_card, flash_card: different_flash_card }, format: :json
+          expect(response).to render_template(:show)
+        end
+      end
+
+      context "with invalid attributes" do
+        it "has status 409" do
+          request.headers.merge! headers
+          patch :update, params: { id: flash_card, flash_card: invalid_flash_card }, format: :json
+          expect(response).to have_http_status(409)
+          expect(response.content_type).to eq("application/json")
+        end
+
+        it "doesn't update the flash card" do
+          request.headers.merge! headers
+          patch :update, params: { id: flash_card, flash_card: different_flash_card }, format: :json
+          expect(assigns(:flash_card).reload.attributes).to eq flash_card.reload.attributes
+        end
+      end
+
+      context "when passed the wrong user's token" do
+        it "has status 401" do
+          request.headers.merge! headers
+          patch :update, params: { id: other_flash_card, user: different_flash_card }, format: :json
+          expect(response).to have_http_status(401)
+        end
+      end
+
+      context "when not passed a valid token" do
+        it "has status 401" do
+          patch :update, params: { id: flash_card, flash_card_set: different_flash_card }, format: :json
+          expect(response).to have_http_status(401)
+        end
+      end
+    end
+  end
 end
