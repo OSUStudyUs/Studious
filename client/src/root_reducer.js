1a9929abadc7936ac91e7e306a16ab6747d811df
Alex Tareshawty <atareshawty@gmail.com>
2016-11-27 21:13:17 -0500

    client: put sidebar into the app

--- client/src/root_reducer.js
+++ client/src/root_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import authentication from './authentication';
+import sidebar from './sidebar';
 
 export default combineReducers({
-  [authentication.constants.NAME]: authentication.reducer
+  [authentication.constants.NAME]: authentication.reducer,
+  [sidebar.constants.NAME]: sidebar.reducer
 });
