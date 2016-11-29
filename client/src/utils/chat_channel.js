fb6ba469c80f6050323e6c0e5e67a8df8a87f522
Alex Tareshawty <atareshawty@gmail.com>
2016-11-29 11:01:51 -0500

    client: create actioncable consumer on user signup also

--- client/src/utils/chat_channel.js
+++ client/src/utils/chat_channel.js
@@ -5,9 +5,9 @@ import jwt from './jwt';
 const protocol = process.env.NODE_ENV === 'development' ? 'ws' : 'wss';
 const port = process.env.NODE_ENV === 'development' ? ':8080' : '';
 
-const actionCableUrl = `${protocol}://${location.hostname}${port}/api/sockets/${jwt.getToken()}`;
+const actionCableUrl = () => `${protocol}://${location.hostname}${port}/api/sockets/${jwt.getToken()}`;
 const initialState = jwt.getToken() ?
-  ActionCable.createConsumer(actionCableUrl) :
+  ActionCable.createConsumer(actionCableUrl()) :
   null;
 
 export const NAME = 'chatChannel';
@@ -17,12 +17,14 @@ export const reducer = (state = initialState, { type, payload }) => {
     // this is really bad... normally, we'd want to import this from authentication
     // but for some reason, import isn't working
     case 'LOGIN_SUCCESS':
-      return ActionCable.createConsumer(`ws://localhost:8080/api/sockets/${payload.token}`);
+      return ActionCable.createConsumer(actionCableUrl());
     case 'LOGOUT_SUCCESS':
       if (state.disconnect) {
         state.disconnect();
       }
       return null;
+    case 'SIGNUP_SUCCESS':
+      return ActionCable.createConsumer(actionCableUrl());    
     default:
       return state;
   }
