3f4d0b77ec0c91f8ac4776cf80515a705d62e032
Alex Tareshawty <atareshawty@gmail.com>
2016-11-28 23:33:43 -0500

    api: transmit json in chat_channel

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -4,9 +4,13 @@ class ChatChannel < ApplicationCable::Channel
   # Author: Kyle Thompson
   # Revisions:
   #   1: 11/28/16 - Kyle Thompson - initial implementation
+  #   2: 11/28/16 - Alex Tareshawty - render json string of message
   def subscribed
     @chatroom = Chatroom.find params[:chatroom_id]
-    stream_for @chatroom, coder: ActiveSupport::JSON
+    stream_for @chatroom, coder: ActiveSupport::JSON do |message|
+      message = Message.includes(:user).find(message['id'])
+      transmit Api::MessagesController.render(partial: 'api/messages/message', locals: { message: message })
+    end
   end
 
   # Public: persists a message to the DB and broadcasts it to the channel
@@ -14,6 +18,7 @@ class ChatChannel < ApplicationCable::Channel
   # Author: Kyle Thompson
   # Revisions:
   #   1: 11/28/16 - Kyle Thompson - initial implementation
+  #   2: 11/28/16 - Alex Tareshawty - update things to suit my needs
   def send_message(data)
     message = current_user.messages.create!(content: data["content"], chatroom_id: @chatroom.id)
     ChatChannel.broadcast_to @chatroom, message
