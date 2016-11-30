163294eca64a91cbdf42d7bffa387b2e4cff40fd
Joel Diener <jzdiener@gmail.com>
2016-11-29 23:32:41 -0500

    LandingPage: initial implementation

--- client/src/shared_components/landing_page.jsx
+++ client/src/shared_components/landing_page.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import auth from '../authentication';
 
+import './landing_page.scss';
+
 const { components } = auth;
 const { Signup } = components;
 
 const LandingPage = ({ actions }) => (
   <div className="LandingPage">
-    This is an unprotected route
-    <Signup onSignup={(user) => actions.signupUser(user)}/>
+    <div className="LandingPageCenteredText">
+      <p>Find academic groups. Chat. Practice flash cards.</p>
+      <h1>Better Together</h1>
+    </div>
+    <div className="LandingPageForm">
+       <Signup onSignup={(user) => actions.signupUser(user)}/>
+    </div>
   </div>
 );
 
