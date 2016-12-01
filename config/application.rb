56b2ae6a0fc8e9c7c787ea164981fe0e965cc3e7
Kyle Thompson <kyle@kylekthompson.com>
2016-12-01 14:07:02 -0500

    api: allow all methods with CORS

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,9 @@ module Studious
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
   end
