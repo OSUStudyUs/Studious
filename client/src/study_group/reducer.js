924f3146e827326070edc670d3943b15e1d60e8f
Alex Tareshawty <atareshawty@gmail.com>
2016-12-02 00:44:06 -0500

    client: profile and study group reducers look to see if a flashcardset was created on their behalf

--- client/src/study_group/reducer.js
+++ client/src/study_group/reducer.js
@@ -2,7 +2,9 @@ import {
   STUDY_GROUP_CREATION_SUCCESS, STUDY_GROUP_LOAD_SUCCESS
 } from './actions';
 import * as actions from '../profile/actions';
+import * as flashCardSetActions from '../flash_card_set/actions';
 
+const { CREATE_FLASH_CARD_SET_SUCCESS } = flashCardSetActions;
 const { PROFILE_LOAD_SUCCESS } = actions;
 const initialState = {
   byId: { }
@@ -12,6 +14,19 @@ const studyGroup = (state = initialState, { type, payload }) => {
   const newState = { ...state };
 
   switch (type) {
+    case CREATE_FLASH_CARD_SET_SUCCESS:
+      const { studyGroupId } = payload.flashCardSet;
+
+      if (newState.byId[studyGroupId]) {
+        newState.byId[studyGroupId].flashCardSets = (newState.byId[studyGroupId].flashCardSets || []).concat({
+          id: payload.flashCardSet.id,
+          name: payload.flashCardSet.name
+        });
+        return newState;
+      } else {
+        return newState;
+      }
+
     case PROFILE_LOAD_SUCCESS:
       const { studyGroups } = payload;
 
