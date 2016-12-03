14e4e881bfbb83eda64a9c45574ac9ac66690b66
Alex Tareshawty <atareshawty@gmail.com>
2016-12-02 20:52:03 -0500

    client: flash messages are chips

--- client/src/flash_message/container.jsx
+++ client/src/flash_message/container.jsx
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import Avatar from 'material-ui/Avatar';
 import { bindActionCreators } from 'redux';
-import { titleCase } from 'change-case';
+import { connect } from 'react-redux';
+import Chip from 'material-ui/Chip';
+import { red200, red800 } from 'material-ui/styles/colors';
 
 import './container.scss';
 import * as actions from './actions';
@@ -18,49 +20,73 @@ class FlashMessage extends Component {
 
   static propTypes = {
     clearErrors: PropTypes.func.isRequired,
-    errors: PropTypes.object,
     loginError: PropTypes.string,
-    resource: PropTypes.string,
     status: PropTypes.number
   };
 
-  render() {
-    const { clearErrors, errors, loginError, resource, status } = this.props;
-    let statusText = null;
+  constructor() {
+    super();
+    this.handleClear = this.handleClear.bind(this);
+  }
 
-    if (!errors && !resource && !status && !loginError) return null;
+  handleClear() {
+    this.props.clearErrors();
+  }
+
+  render() {
+    const { loginError, status } = this.props;
+    let statusText = 'Sorry, there is an issue with your request';
+
+    if (!status && !loginError) return null;
 
     switch (status) {
       case 401:
-        statusText = <span>You aren't authorized to access that resource</span>;
+        statusText = `You aren't authorized to access that resource`;
         break;
       case 404:
-        statusText = <span>Sorry, that resource doesn't exist</span>;
+        statusText = `Sorry, that resource doesn't exist`;
         break;
       case 500:
-        statusText = <span>Sorry, there's a problem with our server. We are aware of the problem and are resolving the issue</span>;
+        statusText = `Sorry, there's a problem with our server. We are aware of the problem and are resolving the issue`;
         break;
       default:
         /* Do Nothing */
         break;
     }
 
+    if (loginError) {
+      return (
+        <div className="FlashMessage">
+          <Chip
+            onRequestDelete={this.handleClear}
+            style={{
+              margin: 4,
+              zIndex: 200
+            }}
+          >
+            <Avatar size={32} color={red200} backgroundColor={red800}>
+              404
+            </Avatar>
+            {loginError}
+          </Chip>
+        </div>
+      );
+    }
+
     return (
       <div className="FlashMessage">
-        {loginError && <span>{loginError}</span>}
-        {resource && <span>There was a problem with your {titleCase(resource)}</span>}
-        {errors &&
-          <div>
-            Errors:
-            {Object.keys(errors).map((key) =>
-              <div key={key}>
-                {errors[key].map((error) => <div key={error}>{key} {error}</div>)}
-              </div>
-            )}
-          </div>
-        }
-        {statusText}
-        <button onClick={clearErrors}>x</button>
+        <Chip
+          onRequestDelete={this.handleClear}
+          style={{
+            margin: 4,
+            zIndex: 200
+          }}
+        >
+          <Avatar size={32} color={red200} backgroundColor={red800}>
+            {status}
+          </Avatar>
+          {statusText}
+        </Chip>
       </div>
     );
   }
